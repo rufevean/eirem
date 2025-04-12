f52499cf1fad7a4cfc4f6552508e8684cddee021
rufevean <deerajmydearalways@gmail.com>
2025-04-12 21:38:14 +0530

    deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0', // 👈 Allow connections from LAN (tablet, phone, etc.)
-    port: 5173,       // 👈 Optional, but you can specify if needed
+    host: '0.0.0.0',
+    port: 5173,
+    allowedHosts: ['ba6a-2401-4900-8899-698a-6748-c8c3-ee5e-a49.ngrok-free.app'],
   },
 })
